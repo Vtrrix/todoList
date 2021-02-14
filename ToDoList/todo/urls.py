c9ef6eaa92506d879dce68cdf48dd8d0a32bd2d4
Vivek <viveksonybcd@gmail.com>
2021-02-14 15:48:44 +0530

    added delete view

--- ToDoList/todo/urls.py
+++ ToDoList/todo/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('' , views.get_todo_list),
-    path('add_task/',views.add_task)
+    path('', views.get_todo_list),
+    path('add_task/', views.add_task),
+    path('delete_task/', views.del_task)
 ]
--- ToDoList/todo/views.py
+++ ToDoList/todo/views.py
@@ -2,14 +2,18 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 import json
+import uuid
 
-
+a = uuid.uuid1()
+b = uuid.uuid1()
+c = uuid.uuid1()
+d = uuid.uuid1()
 obj = {
     "tasks": [
-        {"task": "abc", "checked": False},
-        {"task": "def", "checked": False},
-        {"task": "ghi", "checked": False},
-        {"task": "jkl", "checked": False},
+        {"id": 1, "task": "abc", "checked": False},
+        {"id": 2, "task": "def", "checked": False},
+        {"id": 3, "task": "ghi", "checked": False},
+        {"id": 4, "task": "jkl", "checked": False},
     ]
 }
 
@@ -20,7 +24,6 @@ def get_todo_list(request):
 
 @csrf_exempt
 def add_task(request):
-    success = False
     if request.method == "POST":
         # print(f"{request.POST.items()}")
         # print(dict(request.POST.items()))
@@ -30,4 +33,30 @@ def add_task(request):
         obj['tasks'].append(json.loads(request.POST.get('task_list')))
         # obj['tasks'].append(request.POST.get('task_list'))
         # print(obj)
+    return JsonResponse(obj)
+
+
+def find(lst, value):
+    # print(lst)
+    for i, dic in enumerate(lst):
+        # print(i)
+        print(dic["id"])
+        # print(value)
+        if int(dic['id']) == int(value):
+            print("----------")
+            print(value)
+            print(dic['id'])
+            print("----------")
+            return i
+    return -1
+    # res = [dic['id'] for dic in lst]
+    print(res)
+
+
+@csrf_exempt
+def del_task(request):
+    if request.method == "POST":
+        print(request.POST.get('task_id'))
+        obj['tasks'].remove(
+            obj['tasks'][int(find(obj['tasks'],  request.POST.get('task_id')))])
     return JsonResponse(obj)
