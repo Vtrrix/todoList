d8e9739d48eb125a6061a481936949797ad2af9a
Vivek <viveksonybcd@gmail.com>
2021-02-24 22:42:04 +0530

    bug fix

--- ToDoList/todo/views.py
+++ ToDoList/todo/views.py
@@ -4,62 +4,52 @@ from django.views.decorators.csrf import csrf_exempt, csrf_protect
 import json
 import uuid
 
-a = uuid.uuid1()
-b = uuid.uuid1()
-c = uuid.uuid1()
-d = uuid.uuid1()
 obj = {
     "tasks": [
-        {"id": 1, "task": "abc", "checked": False},
-        {"id": 2, "task": "def", "checked": False},
-        {"id": 3, "task": "ghi", "checked": False},
-        {"id": 4, "task": "jkl", "checked": False},
+        {"id": uuid.uuid1(), "task": "abc", "checked": False},
+        {"id": uuid.uuid1(), "task": "def", "checked": False},
+        {"id": uuid.uuid1(), "task": "ghi", "checked": False},
+        {"id": uuid.uuid1(), "task": "jkl", "checked": False},
     ]
 }
+
+# GET - Get all the tasks
 
 
 def get_todo_list(request):
     return JsonResponse(obj)
 
+# POST - Add a new task and save it to DJANGO server
+
 
 @csrf_exempt
 def add_task(request):
     if request.method == "POST":
-        # print(f"{request.POST.items()}")
-        # print(dict(request.POST.items()))
-        # obj['tasks'].append(dict(request.POST.items()))
-        # print(obj)
         print(request.POST.get('task_list'))
         obj['tasks'].append(json.loads(request.POST.get('task_list')))
-        # obj['tasks'].append(request.POST.get('task_list'))
-        # print(obj)
     return JsonResponse(obj)
+
+# To find element from tasks
 
 
 def find(lst, value):
-    # print(lst)
     for i, dic in enumerate(lst):
-        # print(i)
         print(dic["id"])
-        # print(value)
-        if int(dic['id']) == int(value):
-            print("----------")
-            print(value)
-            print(dic['id'])
-            print("----------")
+        if str(dic['id']) == str(value):
             return i
     return -1
-    # res = [dic['id'] for dic in lst]
-    print(res)
+
+# POST - To delete task by id
 
 
 @csrf_exempt
 def del_task(request):
     if request.method == "POST":
-        print(request.POST.get('task_id'))
         obj['tasks'].remove(
             obj['tasks'][int(find(obj['tasks'],  request.POST.get('task_id')))])
     return JsonResponse(obj)
+
+# POST - To change the status of the task
 
 
 @csrf_exempt
